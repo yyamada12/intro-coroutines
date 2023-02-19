f01ace24d72ce577fead6b3cd2257836082836c9
yyamada12 <12yacropolisy@gmail.com>
2023-02-19 15:44:56 +0900

    task2 implement callback updateResults

--- src/tasks/Request2Background.kt
+++ src/tasks/Request2Background.kt
@@ -3,10 +3,14 @@ package tasks
 import contributors.GitHubService
 import contributors.RequestData
 import contributors.User
+import javax.swing.SwingUtilities
 import kotlin.concurrent.thread
 
 fun loadContributorsBackground(service: GitHubService, req: RequestData, updateResults: (List<User>) -> Unit) {
     thread {
-        loadContributorsBlocking(service, req)
+        val users = loadContributorsBlocking(service, req)
+        SwingUtilities.invokeLater {
+            updateResults(users)
+        }
     }
 }
