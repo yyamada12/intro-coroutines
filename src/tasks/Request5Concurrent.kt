342a3c4bec75a2bde04120839df9be542bada075
yyamada12 <12yacropolisy@gmail.com>
2023-02-19 17:12:10 +0900

    task5 implement parallel async calls

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -4,5 +4,17 @@ import contributors.*
 import kotlinx.coroutines.*
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: emptyList()
+
+    repos.map {repo ->
+        async {
+            log("starting loading for ${repo.name}")
+            service.getRepoContributors(req.org, repo.name)
+                .also {logUsers(repo, it)}
+                .bodyList()
+        }
+    }.awaitAll().flatten().aggregate()
 }
