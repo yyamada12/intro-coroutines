92a6ea8a72c248f5225e5feb863a591c7e88a791
yyamada12 <12yacropolisy@gmail.com>
2023-02-19 18:22:12 +0900

    task7 use channels for showing status

--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -1,9 +1,8 @@
 package tasks
 
 import contributors.*
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 
 suspend fun loadContributorsChannels(
     service: GitHubService,
@@ -11,6 +10,27 @@ suspend fun loadContributorsChannels(
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .body() ?: emptyList()
+
+        val allUsers = mutableListOf<User>()
+        val channel = Channel<List<User>>()
+
+        repos.forEach {repo ->
+            launch {
+                log("starting loading for ${repo.name}")
+                val users = service.getRepoContributors(req.org, repo.name)
+                    .also {logUsers(repo, it)}
+                    .bodyList()
+                channel.send(users)
+            }
+        }
+        repeat(repos.size){ i ->
+            val users = channel.receive()
+            allUsers.addAll(users)
+            updateResults(allUsers.aggregate(), i == repos.lastIndex)
+        }
     }
 }
