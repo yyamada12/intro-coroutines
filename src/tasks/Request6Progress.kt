6e71f9602b37b032ae960a4825f6b9dfc5ab3685
yyamada12 <12yacropolisy@gmail.com>
2023-02-19 18:02:23 +0900

    task6 showing progress

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -7,5 +7,18 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: emptyList()
+
+    val allUsers = mutableListOf<User>()
+    repos.forEachIndexed { i,repo ->
+        val users = service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+        allUsers.addAll(users)
+        updateResults(allUsers.aggregate(),i == repos.lastIndex)
+    }
 }
