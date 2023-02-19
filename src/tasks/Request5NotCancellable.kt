3b88b046ecef55b71e267eccb1cb2e2c33659556
yyamada12 <12yacropolisy@gmail.com>
2023-02-19 17:46:51 +0900

    task5 not cancellable

--- src/tasks/Request5NotCancellable.kt
+++ src/tasks/Request5NotCancellable.kt
@@ -2,8 +2,20 @@ package tasks
 
 import contributors.*
 import kotlinx.coroutines.*
-import kotlin.coroutines.coroutineContext
 
 suspend fun loadContributorsNotCancellable(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: emptyList()
+
+    val deferreds = repos.map { repo ->
+        GlobalScope.async {
+            log("starting loading for ${repo.name}")
+            service.getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }
+    }
+    return deferreds.awaitAll().flatten().aggregate()
 }
